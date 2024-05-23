2c4f90190bf41e43f8e2cd6da5ab5c15b094b6c7
Trung <trung1994@outlook.de>
2024-05-23 10:47:35 +0200

    Cat.java

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,21 +2,23 @@ package com.cc.java;
 
 public class Cat {
 
+    public String name;
+    public String furColor;
+    public int age;    
+    
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
 
-    // this --> Instanzvariable
-    // public void sayHi(){
-    //     System.out.println("Blick von innen: "  + this);
-    // }
-
-    // // this --> Instanzvariable
-    // public void sayHi(){
-    //     App.output("Blick von innen: "  + this);
-    // }
-
-        // Adresse der Objektinstanz vom Typ Cat
+    // Adresse der Objektinstanz vom Typ Cat
        public Cat getInstanceVar(){
             return this;
         }
+
+    
+    
 
 
     
